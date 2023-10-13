c4c91e4858f7e89419ad78ba12ba9f5b3eb047d6
Coding Otaku <contact@codingotaku.com>
2023-10-13 14:55:49 +0100

    more sanitization

--- api/search.py
+++ api/search.py
@@ -2,20 +2,31 @@ from flask import request, jsonify
 from flask_restful import Resource, reqparse
 from models.site_data import db, SearchSiteData, SQL
 import json
+import re
 
 
 class Search(Resource):
+    def _sanitize(self, content):
+        if not content:
+          return
+
+        list_of_chars = [',', '""', '.', '+', "'"]
+        pattern = '[' +  ''.join(list_of_chars) +  ']'
+        return re.sub(pattern, '', content)
+
 
     def get(self):
         content = request.args.get('query')
         site = request.args.get('site')
 
-        if content is None:
-            return jsonify([])
+        content = self._sanitize(content)
+        
+        if content is None or content.strip() == "":
+            return jsonify([])        
 
-        content = content.replace('"', '""')
-        if site is None:
+        if site is None or site.strip() == "":
           site='.'
+
         query = SearchSiteData.search_bm25('{}'.format(content)).where(SearchSiteData.url.contains(site)).limit(10).dicts()
 
         rows=[]
